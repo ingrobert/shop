549b1d47dc3909f15758b3b496815a51b4ca477f
RI <rmi22186+github@gmail.com>
2016-11-29 04:26:29 +0000

    remove redux-promise from app

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
